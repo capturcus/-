1fee79b17138c4564a94bfb74728df191da832fe
Marcin Parafiniuk <consommepanchi@gmail.com>
2026-05-31 14:49:57 +0200

    actual hindley milner

--- gćć-python/typechecker.py
+++ gćć-python/typechecker.py
@@ -1,13 +1,46 @@
 import ast_nodes as ast
 import re
+from dataclasses import dataclass, field
 
 last_type = 0
 type_regex = re.compile(r"t[0-9]+")
+
 def new_type():
     global last_type
     ret = "t"+str(last_type)
     last_type += 1
     return ret
+
+all_types = {}
+
+def find_type(t):
+    global all_types
+    global type_regex
+    if not type_regex.match(t):
+        # concrete type
+        return t
+    if not t in all_types.keys():
+        all_types[t] = t
+        return t
+    v = all_types[t]
+    if v == t:
+        # end of tree
+        return v
+    return find_type(v)
+
+def unify_types(t0, t1):
+    global type_regex
+    global all_types
+    ft0 = find_type(t0)
+    ft1 = find_type(t1)
+    if not type_regex.match(ft0) and not type_regex.match(ft1):
+        if ft0 == ft1:
+            return ft0
+        print(f"cannot unify {ft0} with {ft1}")
+        raise
+    concrete, abstract = (ft0, ft1) if type_regex.match(ft1) else (ft1, ft0)
+    all_types[abstract] = concrete
+    return concrete
 
 class Scope:
     def __init__(self):
@@ -25,87 +58,51 @@ class Scope:
         self.types.append((identifier, new_t))
         return new_t
 
+@dataclass
+class FunDefTypes:
+    name: ast.FunctionIdentifier
+    arg_types: list
+    ret_type: str
 
-    def unify(self, t0, t1):
-        if t0 == t1:
-            return
-        global type_regex
-        if not type_regex.match(t0) and not type_regex.match(t1):
-            print(f"cannot unify {t0} with {t1}")
-            raise
-        concrete = t1 if type_regex.match(t0) else t0
-        new_types = []
-        for k in self.types:
-            v = k[0]
-            t = k[1]
-            if t == t0 or t == t1:
-                new_types.append((v, concrete))
-            else:
-                new_types.append((v, t))
-        self.types = new_types
-        return concrete
+fun_decls = []
 
-
-    def unify_other(self, other, my_t, other_t):
-        if my_t == other_t:
-            return
-        global type_regex
-        if not type_regex.match(my_t) and not type_regex.match(other_t):
-            print(f"cannot unify_other {my_t} with {other_t}")
-            raise
-        concrete = other_t if type_regex.match(my_t) else my_t
-        new_types = []
-        for i in self.types:
-            v = i[0]
-            t = i[1]
-            if t == my_t:
-                new_types.append((v, concrete))
-            else:
-                new_types.append((v, t))
-        self.types = new_types
-        new_types = []
-        for i in other.types:
-            v = i[0]
-            t = i[1]
-            if t == other_t:
-                new_types.append((v, concrete))
-            else:
-                new_types.append((v, t))
-        other.types = new_types
-        return concrete
-
-fun_scopes = []
-module = None
+def find_fdt(func_id):
+    global fun_decls
+    for (name, fdt) in fun_decls:
+        if name.lemmas_set & func_id.lemmas_set:
+            return fdt
 
 def resolve_module(node):
     print("Module")
-    global module
-    module = node
-    global fun_scopes
+    global fun_decls
+    fun_scopes = []
     for decl in node.body:
         if isinstance(decl, ast.FunctionDef):
-            fun_name = decl.name.lemmas_set
-            scope = Scope()
+            fdt = FunDefTypes(
+                name=decl.name,
+                arg_types=[new_type() for _ in range(len(decl.params))],
+                ret_type=new_type()
+            )
+            i = 0
             for p in decl.params:
-                scope.types.append((p.name, new_type() if p.type is None else "".join(p.type)))
-            scope.types.append((
-                ast.Identifier(
-                    surface=("__return_type",),
-                    variants=(ast.Variant(
-                        lemmas=("__return_type",), case=frozenset(),
-                        number=None, gender=None, rest_length=0,
-                    ),),
-                ),
-                new_type() if decl.return_type is None else "".join(decl.return_type)
-            ))
-            fun_scopes.append((fun_name, scope))
-    i = 0
+                if not p.type is None:
+                    unify_types(fdt.arg_types[i], "".join(p.type))
+                i += 1
+            if not decl.return_type is None:
+                unify_types(fdt.ret_type, "".join(decl.return_type))
+            fun_decls.append((decl.name, fdt))
     for decl in node.body:
         if isinstance(decl, ast.FunctionDef):
-            resolve_function_def(decl, fun_scopes[i][1])
-            i += 1
-    for s in fun_scopes:
-        print(s[0], s[1].types)
+            scope = Scope()
+            fun_scopes.append(scope)
+            resolve_function_def(decl, scope)
+    for scope in fun_scopes:
+        print("===")
+        for i in scope.types:
+            v = i[0]
+            t = i[1]
+            print("")
+            print(v, find_type(t))
 
 
 def resolve_function_def(node, scope):
@@ -136,7 +133,7 @@ def resolve_expression(node, scope):
     if isinstance(node, ast.Typed):
         expr_t = resolve_expression(node.expr, scope)
         explicit_t = "".join(node.type)
-        return scope.unify(expr_t, explicit_t)
+        return unify_types(expr_t, explicit_t)
     if isinstance(node, ast.BinOp):
         return resolve_bin_op(node, scope)
     if isinstance(node, ast.UnaryOp):
@@ -170,7 +167,7 @@ def resolve_assignment(node, scope):
     print("Assignment")
     target_type = resolve_expression(node.target.resolved, scope)
     value_type = resolve_expression(node.value.resolved, scope)
-    scope.unify(target_type, value_type)
+    unify_types(target_type, value_type)
     # # target to krotka — element pojedynczy lub łańcuch getterów
     # if isinstance(node.target, tuple):
     #     for t in node.target:
@@ -237,36 +234,21 @@ def resolve_or(node, scope):
     resolve_expression(node.left, scope)
     resolve_expression(node.right, scope)
 
-def fun_scope_for_lemmas_set(lemmas_set):
-    global fun_scopes
-    for f in fun_scopes:
-        name = f[0]
-        fun_scope = f[1]
-        print(name)
-        for l in lemmas_set:
-            if l in name:
-                return fun_scope
-    raise "scope not found"
-
-def fun_decl_for_lemmas_set(lemmas_set):
-    global module
-    for node in module.body:
-        if isinstance(node, ast.FunctionDef):
-            fun_name = node.name.lemmas_set
-            if fun_name == lemmas_set:
-                return node
-    raise "decl not found"
-
 
 def resolve_function_call(node, scope):
     print("FunctionCall")
-    fun_scope = fun_scope_for_lemmas_set(node.name.lemmas_set)
-    fun_decl = fun_decl_for_lemmas_set(node.name.lemmas_set)
-    i = 0
-    for p in node.params:
-        t = resolve_expression(p, scope)
-        scope.unify_other(fun_scope, t, fun_scope.get_type(fun_decl.params[i].name))
-        i += 1
+    fdt = find_fdt(node.name)
+    for (t0, p) in zip(fdt.arg_types, node.params):
+        t1 = resolve_expression(p, scope)
+        unify_types(t0, t1)
+    return fdt.ret_type
+    # fun_scope = fun_scope_for_lemmas_set(node.name.lemmas_set)
+    # fun_decl = fun_decl_for_lemmas_set(node.name.lemmas_set)
+    # i = 0
+    # for p in node.params:
+    #     t = resolve_expression(p, scope)
+    #     scope.unify_other(fun_scope, t, fun_scope.get_type(fun_decl.params[i].name))
+    #     i += 1
 
 
 def resolve_getter_chain(node, scope):
